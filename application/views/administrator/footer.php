71fd40082a7e234ccc02b212acc0145aa90ffbdd
RifqiDev <muhammad45rifki@gmail.com>
2021-06-17 17:22:20 +0700

    update apps

--- application/views/administrator/footer.php
+++
@@ -1,2 +0,0 @@
-
-<strong>Copyright &copy; 2016 - <?php echo date('Y'); ?> <a target='_BLANK' href="https://members.phpmu.com"> Marketplace PRO</a>.</strong> All rights reserved. 
--- application/views/phpmu-marketrancak/reseller/view_city.php
+++
@@ -1,5 +0,0 @@
-<?php
-  echo "<option value=''>- Pilih -</option>";
-  foreach ($kota as $row){
-      echo "<option value='$row[kota_id]'>$row[nama_kota]</option>";
-  }
